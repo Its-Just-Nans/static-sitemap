66bab41ab2bd9bf7d1d7cc030d5caa474b860c94
n4n5 <its.just.n4n5@gmail.com>
2025-03-23 18:06:00 +0100

    update

--- README.md
+++ README.md
@@ -18,9 +18,7 @@ python -m static_sitemap https://its-just-nans.github.io/
 - `--verbose` : Display URLs
 - `--quiet` : Display Nothing
 - `--sitemap` : Create a `sitemap.xml` file
-- `--rmError` : Delete incorrect link in the arra
-- `--css` : check css file (useless)
-- `--json` : check json file (useless)
+- `--rm-error` : Delete link with request error
 - `--html` : to display the html from requests
 
 ## Others files
--- static_sitemap/__init__.py
+++ static_sitemap/__init__.py
@@ -1,172 +1,69 @@
 """static sitemap generator"""
 
-from sys import argv
-from re import findall
-from urllib.parse import urlparse
-from datetime import datetime
-from requests import get
-
-NEWS = 'xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"'
-XHTML = ' xmlns:xhtml="http://www.w3.org/1999/xhtml"'
-IMAGE = 'xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"'
-VIDEO = 'xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"'
-
-in_domain = []
-ext = []
-mail = []
-globalVar = {}
+from .lib import (
+    get_displayer,
+    explore_url,
+    disp_results,
+    write_sitemap,
+    parse_args,
+    should_check_file,
+)
 
 
-def disp(text, end=""):
-    """display text"""
-    if not globalVar["quiet"]:
-        if end == "\n":
-            print(text)
-        else:
-            print(text, end="")
-
-
-def parse_args():
-    """parse args"""
-    globalVar["verbose"] = True if "--verbose" in argv else False
-    globalVar["quiet"] = True if "--quiet" in argv else False
-    globalVar["sitemap"] = True if "--sitemap" in argv else False
-    globalVar["rmError"] = True if "--rmError" in argv else False
-    globalVar["css"] = False if "--css" in argv else True
-    globalVar["json"] = False if "--json" in argv else True
-    globalVar["html"] = True if "--html" in argv else False
-
-
-def image(uri):
-    """check if image"""
-    img_ext = ["png", "jpg", "ico", "gif", "jpeg", "svg"]
-    for one_ext in img_ext:
-        if uri.endswith(one_ext):
-            return True
-    return False
-
-
-def explorer(url, domain_name):
-    """explore url"""
-    temp_url = urlparse(url).geturl()
-    try:
-        response = get(temp_url, timeout=5, allow_redirects=True)
-    except Exception as exception:
-        disp(type(exception).__name__, "\n")
-        disp(exception.__class__.__name__, "\n")
-        disp(exception.__class__.__qualname__, "\n")
-        return False
-    web_content = response.text.strip()
-    if globalVar["html"]:
-        disp(web_content, "\n")
-    variable = findall('href="[^"]*"', web_content)
-    without_last = "/".join(response.url.split("/")[:-1])
-    global_domain = urlparse(url).scheme + "://" + urlparse(url).netloc
-    for one_link in variable:
-        one_link = one_link.replace("href=", "")
-        one_link = one_link.replace(one_link[0], "")
-        # sort
-        if one_link.startswith("mailto:"):
-            mail.append(one_link.replace("mailto:", ""))
-        elif one_link.startswith("#"):
-            continue
-        elif one_link.startswith("?"):
-            one_link = response.url.split("?")[0] + one_link
-        elif one_link.startswith("./"):
-            if response.url.endswith("/"):
-                one_link = response.url + one_link[2:]
-            else:
-                one_link = without_last + one_link[1:]
-        elif one_link.startswith("//"):
-            one_link = one_link.replace("//", "http://")
-        elif one_link.startswith("/"):
-            one_link = global_domain + one_link
-
-        if one_link.startswith(domain_name):
-            if one_link not in in_domain:
-                in_domain.append(one_link)
-        elif one_link not in ext:
-            ext.append(one_link)
-
-
-def main():
+def main(base_url, global_vars):
     """global main function"""
-    if len(argv) <= 1:
-        disp("No arguments were given")
-        exit(1)
-    else:
-        base_url = argv[1]
+    if base_url == "":
+        return
+    disp = get_displayer(global_vars)
+    href_in_domain = set()
+    href_out_domain = set()
+    href_mail = set()
+    href_phone = set()
     parse_args()
     domain_name = base_url.split("/")
     while len(domain_name) > 3:
         domain_name.pop()
     domain_name = "/".join(domain_name)
     domain_name += "/"
+    urls_explored = {}
+    urls_list = [base_url]
     try:
-        in_domain.append(base_url)
-        for one_link in in_domain:
-            disp("Exploring " + one_link, "\n")
-            check_file = True
-            if image(one_link):
-                check_file = False
-            elif one_link.endswith("css"):
-                if globalVar["css"]:
-                    check_file = True
+        idx = 0
+        while idx < len(urls_list):
+            current_url = urls_list[idx]
+            idx += 1
+            if should_check_file(current_url, global_vars):
+                rest = explore_url(current_url, domain_name, global_vars)
+                if rest is False and global_vars["--rm-error"]:
+                    href_in_domain.remove(current_url)
                 else:
-                    check_file = False
-            elif one_link.endswith("json"):
-                if globalVar["json"]:
-                    check_file = True
-                else:
-                    check_file = False
-            if check_file:
-                rest = explorer(one_link, domain_name)
-                if rest is False and globalVar["rmError"]:
-                    in_domain.remove(one_link)
+                    for one_link in rest[0]:
+                        if one_link not in urls_explored:
+                            urls_list.append(one_link)
+                            urls_explored[one_link] = False
+                    href_in_domain.add(current_url)
+                    href_out_domain.update(rest[1])
+                    href_mail.update(rest[2])
+                    href_phone.update(rest[3])
+            urls_explored[current_url] = True
+
     except KeyboardInterrupt:
         pass
     except Exception as _exception:
         pass
 
-    if not globalVar["quiet"]:
+    if not global_vars["quiet"]:
         disp("Finished Exploring", "\n")
 
-    if globalVar["sitemap"]:
-        string_to_add = ""
-        string_to_add += '<?xml version="1.0" encoding="UTF-8"?>' + "\n"
-        string_to_add += (
-            f'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" {NEWS} {XHTML} {IMAGE} {VIDEO}>'
-            + "\n"
-        )
-        for one_link in in_domain:
-            string_to_add += "    <url>\n"
-            string_to_add += f"{' '*8}<loc>{one_link}</loc>\n"
-            string_to_add += f"{' '*8}<changefreq>weekly</changefreq>\n"
-            string_to_add += f"{' '*8}<priority>0.5</priority>\n"
-            string_to_add += "    </url>\n"
-        string_to_add += "</urlset>"
-        with open("sitemap.xml", "w", encoding="utf-8") as f:
-            f.write(string_to_add)
+    if global_vars["sitemap"]:
+        write_sitemap(href_in_domain)
 
-    disp(">>> Internal : ", "")
-    disp(len(in_domain), "\n")
-    if globalVar["verbose"]:
-        for one_link in in_domain:
-            disp(one_link, "\n")
-
-    disp(">>> External : ", "")
-    disp(len(ext), "\n")
-    if globalVar["verbose"]:
-        for one_link in ext:
-            disp(one_link, "\n")
-
-    disp(">>> Mails : ", "")
-    disp(len(mail), "\n")
-    if globalVar["verbose"]:
-        for one_link in mail:
-            disp(one_link, "\n")
-
-
-if __name__ == "__main__":
-    parse_args()
-    main()
+    disp_results(
+        global_vars,
+        {
+            "href_in_domain": href_in_domain,
+            "href_out_domain": href_out_domain,
+            "href_mail": href_mail,
+            "href_phone": href_phone,
+        },
+    )
